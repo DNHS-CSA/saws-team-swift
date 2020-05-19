93e76b9bd9dfc9adecc634d50031cc5946819d8a
mhayescs19 <mhayescs19@gmail.com>
2020-05-18 20:52:28 -0700

    added functionality where ingredients are at top of UI in the game screen!

--- stackFoodGame/Control/playGameDictionaries.swift
+++ stackFoodGame/Control/playGameDictionaries.swift
@@ -12,7 +12,7 @@ import UIKit
  Location Spawns:
  pointL2:(x: 45.0), pointL1:(x: 125.0), cPoint:(x: 215.0), pointR1:(x: 310.0), pointR2:(x:370.0)
  */
-var locationSpawns: [CGPoint] = [CGPoint(x: 45.0, y: 90), CGPoint(x: 125.0, y: 90.0), CGPoint(x: 215.0, y: 90.0), CGPoint(x: 310.0, y: 90.0), CGPoint(x: 370.0, y: 90.0)]
+var locationSpawns: [CGPoint] = [CGPoint(x: 45.0, y: 80.0), CGPoint(x: 125.0, y: 80.0), CGPoint(x: 215.0, y: 80.0), CGPoint(x: 310.0, y: 80.0), CGPoint(x: 370.0, y: 80.0)]
 
 var ingredientTypes: [String] = ["cheese", "burger", "lettuce", "tomato", "onions", "mustard", "ketchup", "topBun"]
 
