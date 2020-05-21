eaa6501884eddc806512e77b72fcb9a6dd6b314a
mhayescs19 <mhayescs19@gmail.com>
2020-05-20 22:17:36 -0700

    empty table view with constraints set

---
+++ stackFoodGame/View Controllers/GameHistoryController.swift
@@ -0,0 +1,30 @@
+//
+//  GameHistoryController.swift
+//  stackFoodGame
+//
+//  Created by Michael Hayes on 5/20/20.
+//  Copyright © 2020 Michael Hayes. All rights reserved.
+//
+
+import UIKit
+
+class GameHistoryController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
