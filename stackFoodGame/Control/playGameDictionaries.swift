3d14e0dcddc675a54ae8d1ebc65bbe9056fd5ee9
mhayescs19 <mhayescs19@gmail.com>
2020-05-24 20:55:20 -0700

    fixed issue in playGame: eliminated "floating" ingredients due to wrong top bun name in reference array of names, adjusted console output in playGame

--- stackFoodGame/Control/playGameDictionaries.swift
+++ stackFoodGame/Control/playGameDictionaries.swift
@@ -14,7 +14,7 @@ import UIKit
  */
 var locationSpawns: [CGPoint] = [CGPoint(x: 45.0, y: 80.0), CGPoint(x: 125.0, y: 80.0), CGPoint(x: 215.0, y: 80.0), CGPoint(x: 310.0, y: 80.0), CGPoint(x: 370.0, y: 80.0)]
 
-var ingredientTypes: [String] = ["cheese", "burger", "lettuce", "tomato", "onions", "mustard", "ketchup", "topBun"]
+var ingredientTypes: [String] = ["cheese", "burger", "lettuce", "tomato", "onions", "mustard", "ketchup", "topbun"]
 
 var orderIngredientType: [String] = ["cheese", "burger", "lettuce", "tomato", "onions", "mustard", "ketchup"]
 
